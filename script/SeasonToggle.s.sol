83ec9b864980a872a581ca465b656e9416773391
spengrah <code@spengrah.xyz>
2023-04-25 16:44:11 -0500

    fix formatting

--- script/SeasonToggle.s.sol
+++ script/SeasonToggle.s.sol
@@ -34,7 +34,13 @@ contract DeployFactory is Script {
   }
   // forge script script/SeasonToggle.s.sol:DeployFactory -f mainnet --broadcast --verify
 
-  // forge verify-contract --chain-id 5 --num-of-optimizations 1000000 --watch --constructor-args $(cast abi-encode "constructor(string)" "0.1.0") --compiler-version v0.8.18 0xbD881534a2cD11eB7F3BC70ceDc65d435F9181a1 src/SeasonToggle.sol:SeasonToggle --etherscan-api-key $ETHERSCAN_KEY
-  
-  // forge verify-contract --chain-id 5 --num-of-optimizations 1000000 --watch --constructor-args $(cast abi-encode "constructor(address,address,string)" 0xbD881534a2cD11eB7F3BC70ceDc65d435F9181a1 0x9D2dfd6066d5935267291718E8AA16C8Ab729E9d "0.1.0") --compiler-version v0.8.18 0xa2003399a08a3638af97405cd5f6a6d958cbfb51 src/SeasonToggleFactory.sol:SeasonToggleFactory --etherscan-api-key $ETHERSCAN_KEY
+  // forge verify-contract --chain-id 5 --num-of-optimizations 1000000 --watch --constructor-args $(cast abi-encode
+  // "constructor(string)" "0.1.0") --compiler-version v0.8.18 0xbD881534a2cD11eB7F3BC70ceDc65d435F9181a1
+  // src/SeasonToggle.sol:SeasonToggle --etherscan-api-key $ETHERSCAN_KEY
+
+  // forge verify-contract --chain-id 5 --num-of-optimizations 1000000 --watch --constructor-args $(cast abi-encode
+  // "constructor(address,address,string)" 0xbD881534a2cD11eB7F3BC70ceDc65d435F9181a1
+  // 0x9D2dfd6066d5935267291718E8AA16C8Ab729E9d "0.1.0") --compiler-version v0.8.18
+  // 0xa2003399a08a3638af97405cd5f6a6d958cbfb51 src/SeasonToggleFactory.sol:SeasonToggleFactory --etherscan-api-key
+  // $ETHERSCAN_KEY
 }
