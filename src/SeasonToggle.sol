e92c182045389974d0bff8fbda324f2e6d2d79ab
Ido <gershido@gmail.com>
2023-05-29 13:31:21 +0300

    add immutable args documentation

--- src/SeasonToggle.sol
+++ src/SeasonToggle.sol
@@ -38,6 +38,26 @@ contract SeasonToggle is HatsToggleModule {
   /*//////////////////////////////////////////////////////////////
                           INTERNAL  CONSTANTS
   //////////////////////////////////////////////////////////////*/
+  /**
+   * This contract is a clone with immutable args, which means that it is deployed with a set of
+   * immutable storage variables (ie constants). Accessing these constants is cheaper than accessing
+   * regular storage variables (such as those set on initialization of a typical EIP-1167 clone),
+   * but requires a slightly different approach since they are read from calldata instead of storage.
+   *
+   * Below is a table of constants and their locations. In this module, all are inherited from HatsModule.
+   *
+   * For more, see here: https://github.com/Saw-mon-and-Natalie/clones-with-immutable-args
+   *
+   * --------------------------------------------------------------------+
+   * CLONE IMMUTABLE "STORAGE"                                           |
+   * --------------------------------------------------------------------|
+   * Offset  | Constant        | Type    | Length  | Source Contract     |
+   * --------------------------------------------------------------------|
+   * 0       | IMPLEMENTATION  | address | 20      | HatsModule          |
+   * 20      | HATS            | address | 20      | HatsModule          |
+   * 40      | hatId           | uint256 | 32      | HatsModule          |
+   * --------------------------------------------------------------------+
+   */
 
   /**
    * @notice The divisor used to calculate the extension delay proportion given an `extensionDelay` numerator
